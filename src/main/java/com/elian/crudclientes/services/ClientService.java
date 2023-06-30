9e543976bba613ea395d984b4d43c9e539c371be
Elian Silva <you@example.com>
2023-06-30 16:11:00 -0300

    GET clients

--- src/main/java/com/elian/crudclientes/services/ClientService.java
+++ src/main/java/com/elian/crudclientes/services/ClientService.java
@@ -4,6 +4,8 @@ import com.elian.crudclientes.dto.ClientDTO;
 import com.elian.crudclientes.entities.Client;
 import com.elian.crudclientes.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,5 +19,11 @@ public class ClientService {
     public ClientDTO findById(Long id) {
         Client result = repository.findById(id).get();
         return new ClientDTO(result);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        Page<Client> result = repository.findAll(pageable);
+        return result.map(x -> new ClientDTO(x));
     }
 }
